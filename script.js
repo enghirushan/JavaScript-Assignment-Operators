ec35a12ce0b72d881dd4a86406aa2a53d30e91da
Ilangasinghe I M H H <102861747+enghirushan@users.noreply.github.com>
2024-02-25 15:42:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// = operator
+let a = 10;
+let b = 5;
+
+// += operator
+a += 5;
+console.log(a);
+
+// -= operator
+a -= 5;
+console.log(a);
+
+//*= operator
+a *= 5;
+console.log(a);
+
+// /= operator
+a /= 5;
+console.log(a);
+
+// %= operator
+a %= 5;
+console.log(a);
+
+// **= operator
+b**= 2;  // b = b ** 2
+console.log(b);
